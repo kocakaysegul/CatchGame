6de993334331c33ebf1bbb95441181976e77a0eb
kocakaysegul <aysegulkocak112@gmail.com>
2023-02-23 11:43:09 +0300

    replay function completed

--- CatchGame/ViewController.swift
+++ CatchGame/ViewController.swift
@@ -116,7 +116,16 @@ class ViewController: UIViewController {
             let alert = UIAlertController(title: "Time's up!", message: "Do you want to play again?", preferredStyle: UIAlertController.Style.alert)
             let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel)
             let replayButoon  = UIAlertAction(title: "Replay", style: UIAlertAction.Style.default) { UIAlertAction in
+                
                 //Replay function
+                
+                self.score = 0
+                self.scoreLabel.text = "Score: \(self.score)"
+                self.counter = 10
+                self.timeLabel.text = String(self.counter)
+                
+                self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.countDown), userInfo: nil, repeats: true)
+                self.hideTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(self.hideKenny), userInfo: nil, repeats: true)
             }
             
             alert.addAction(okButton)
